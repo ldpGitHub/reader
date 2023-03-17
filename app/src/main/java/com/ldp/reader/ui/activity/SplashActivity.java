f5300ab37a6fd3948f8896636d32ffccc80bb37a
李东平 <2369906157@qq.com>
2023-03-17 13:51:15 +0800

    修改启动页

--- app/src/main/java/com/ldp/reader/ui/activity/SplashActivity.java
+++ app/src/main/java/com/ldp/reader/ui/activity/SplashActivity.java
@@ -1,26 +1,20 @@
 package com.ldp.reader.ui.activity;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.ImageView;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.widget.TextView;
 
 import com.blankj.utilcode.util.BarUtils;
 import com.blankj.utilcode.util.PermissionUtils;
 import com.blankj.utilcode.util.ToastUtils;
 import com.ldp.reader.R;
-import com.ldp.reader.utils.PermissionsChecker;
 
-import java.util.List;
-
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
@@ -34,10 +28,6 @@ public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        BarUtils.transparentStatusBar(this);
-        setContentView(R.layout.activity_splash);
-        unbinder = ButterKnife.bind(this);
         PermissionUtils.permission(Manifest.permission.POST_NOTIFICATIONS, Manifest.permission.WRITE_EXTERNAL_STORAGE).callback((isAllGranted, granted, deniedForever, denied) -> {
             if (isAllGranted) {
                 skipToMain();
@@ -53,6 +43,11 @@ public class SplashActivity extends AppCompatActivity {
             }
 
         }).request();
+        super.onCreate(savedInstanceState);
+        BarUtils.transparentStatusBar(this);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+        unbinder = ButterKnife.bind(this);
     }
 
 
